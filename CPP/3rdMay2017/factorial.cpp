3a996be4b7d42a1079dd475e92591e294b417329
RishuGoenka <goenkarishunew@gmail.com>
2017-05-03 17:27:05 +0530

    Code updated

--- CPP/3rdMay2017/factorial.cpp
+++ CPP/3rdMay2017/factorial.cpp
@@ -1,15 +1,19 @@
 #include<stdio.h>
 #include<conio.h>
+
 void main()
 {
-clrscr();
-int fact=1,i,n;
-printf("\n enter n:");
-scanf("%d",&n);
-for(i=1;i<=n;i++)
-{
-fact=fact*i;
-}
-printf("\n Factorial of %d is %d",n,fact);
-getch();
+	int n,i,f=1;
+	clrscr();
+	printf("\nEnter a number: ");
+	scanf("%d",&n);  	//5
+
+	for(i=n;i>=1;i--)	//5-1
+	{
+		f=f*i;
+	}
+
+	printf("\nFactorial of %d is %d",n,f);
+
+	getch();
 }
